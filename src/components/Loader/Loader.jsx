bbcbaf2c2a9c720c03aa69d76d58344c7ee79f31
DaryPet <d7akkord@gmail.com>
2024-04-17 14:46:02 +0200

    hw-4_styles

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import css from "./Loader.module.css";
 
 export default function LoaderComponent() {
   return (
-    <div>
+    <div className={css.loader}>
       <RotatingLines
         visible={true}
         height="96"
